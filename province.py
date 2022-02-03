9ca4524afb986ebbfee9b944be9873e02a525818
thegister <gian@cercena.com>
2022-02-03 00:06:27 -0500

    provinces.....

--- province.py
+++ province.py
@@ -7,15 +7,31 @@ class province:
         self.type = type
         self.abbr = abbr
 
+    def get_info(self):
+        print(self.name, self.type, self.abbr)
+
+    def get_name(self):
+        print(self.name)
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_type(self):
+        print(type)
+
+    def set_type(self, type):
+        self.type = type
+
+    def get_abbr(self):
+        print(self.abbr)
+
+    def set_abbr(self, abbr):
+        self.abbr = abbr
+
+    
 
 
 
 
 
 
-
-
-
-
-
-        
